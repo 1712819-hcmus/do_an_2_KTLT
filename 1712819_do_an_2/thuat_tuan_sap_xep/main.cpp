a74b2ee5ecb8add607a351169ec0ff9390181149
1712819-hcmus <37909676+1712819-hcmus@users.noreply.github.com>
2018-07-10 11:22:05 +0700

    sort

--- 1712819_do_an_2/thuat_tuan_sap_xep/main.cpp
+++ 1712819_do_an_2/thuat_tuan_sap_xep/main.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+﻿#include<iostream>
 #include<time.h>
 #include <stdlib.h>
 using namespace std;
@@ -11,9 +11,9 @@ void aray(int *a, int n)
 void xuat(int *a, int n)
 {
 	for (int i = 0; i < n; i++)
-		cout << a[i]<<"  ";
+		cout << *(a+i)<<"   ";
 }
-/*void SWAP(int &a, int &b)
+void SWAP(int &a, int &b)
 {
 	int temp;
 	temp =a;
@@ -23,13 +23,21 @@ void xuat(int *a, int n)
 void SelectionSort(int a[], int n)
 {
 	int i, j, pos;
-	for (int i = 0; i < n - 1; i++) {
+	for ( i = 0; i < n - 1; i++) {
 		pos = i;
-		for (int j = i + 1; j < n; j++)
+		for ( j = i + 1; j < n; j++)
 			if (a[j] < a[pos])
 				pos = j;
 		SWAP(a[i], a[pos]);
 	}
+}
+void dummySelection(int a[], int n)
+{
+	int i, j;
+	for (i = 0; i < n - 1; i++)
+		for (j = i + 1; j < n; j++)
+			if (a[i] > a[j])
+				SWAP(a[i], a[j]);
 }
 void InsertionSort(int a[], int n)
 {
@@ -46,7 +54,7 @@ void InsertionSort(int a[], int n)
 		a[j + 1] = x;
 	}
 }
-void InsertionSortwithSentine(int a[], int n)
+void InsertionSortwithSentine(int a[], int n)//lỗi
 {
 	int x, i, j;
 	for (i = 2; i <= n; i++)
@@ -91,7 +99,7 @@ void BubbSort(int a[], int n)
 			if (a[j - 1] > a[j])
 				SWAP(a[j - 1], a[j]);
 }
-void ShakeSort(int a[], int n)
+void ShakeSort(int a[],int n)
 {
 	int left, right, j, k;
 	left = 1;
@@ -110,15 +118,132 @@ void ShakeSort(int a[], int n)
 			}
 		right = k - 1;
 	} while (left <= right);
-}*/
+}
+void SelectionSort(int a[], int n, int &countAssign, int &countcompare)
+{
+	countAssign = 0;
+	countcompare = 0;
+	int i, j, pos;
+	for ( i = 0, countAssign++;++countcompare && i < n - 1 ; i++, countAssign++) 
+	{
+		pos = i;
+		countAssign++;
+		for ( j = i + 1, countAssign++; ++countcompare && j < n; j++, countAssign++)
+			if (++countcompare && a[j] < a[pos]) {
+				pos = j;
+				countAssign++;
+			}
+		SWAP(a[i], a[pos]);
+	}
+}
+void InsertionSort(int a[], int n, int &countAssign, int &countcompare)
+{
+	countAssign = 0;
+	countcompare = 0;
+	int x, i, j;
+	for (i = 1, countAssign++; ++countcompare && i < n; i++, countAssign++)
+	{
+		x = a[i];
+		j = i - 1;
+		countAssign = +2;
+		while (++countcompare && j >= 0 && ++countcompare  && a[j] > x)//*
+		{
+			a[j + 1] = a[j];
+			j--;
+			countAssign = +2;
+		}
+		a[j + 1] = x;
+		countAssign++;
+	}
+}
+void BinaryInsertionSort(int a[], int n, int &countAssign, int &countcompare)
+{
+	countAssign = 0;
+	countcompare = 0;
+	int i, j, left, right, mid, x;
+	for (i = 1, countAssign++ ;++countcompare && i < n; i++, countAssign++)
+	{
+		x = a[i];
+		left = 0;
+		right = i - 1;
+		countAssign = +3;
+		while (++countcompare && left <= right)
+		{
+			mid = (left + right) / 2;
+			countAssign++;
+			if (++countcompare && x < a[mid])
+			{
+				right = mid - 1;
+				countAssign++;
+			}
+			else
+			{
+				left = mid + 1;
+				countAssign++;
+			}
+		}
+		for (j = i - 1, countAssign++ ; ++countcompare && j >= left; j--, countAssign++)
+		{
+			a[j + 1] = a[j];
+			countAssign++;
+		}
+		a[left] = x;
+		countAssign++;
+	}
+}
+void BubbSort(int a[], int n, int &countAssign, int &countcompare)
+{
+	countAssign = 0;
+	countcompare = 0;
+	int i, j;
+	for (i = 1, countAssign++; ++countcompare && i < n; i++, countAssign++)
+		for (j = n - 1, countAssign++; ++countcompare && j >= i; j--, countAssign++)
+			if (++countcompare && a[j - 1] > a[j])
+				SWAP(a[j - 1], a[j]);
+}
+void ShakeSort(int a[], int n,int &countAssign,int &countcompare)
+{
+	countAssign = 0;
+	countcompare = 0;
+	int left, right, j, k;
+	left = 1;
+	right = n - 1;
+	countAssign += 2;
+	do {
+		for (j = right, countAssign++; ++countcompare && j >= left ; j--, countAssign++)
+			if (++countcompare && a[j - 1] > a[j] ) {
+				SWAP(a[j], a[j - 1]);
+				k = j;
+				countAssign++;
+			}
+		left = k + 1;
+		countAssign++;
+		for (j = left, countAssign++ ; ++countcompare && j <= right ; j++, countAssign++)
+			if (++countcompare && a[j - 1] > a[j] ) {
+				SWAP(a[j], a[j - 1]);
+				k = j;
+				countAssign++;
+			}
+		right = k - 1;
+		countAssign++;
+	} while (++countcompare && left <= right );
+}
 int main()
 {
-	int n ;
+	int n,coutAssign1,countcompera1 ;
 	cout << "Nhap vao so phan tu: ";
 	cin >> n;
 	int *a = new int[n];
 	aray(a, n);
-	xuat(a, n);
+	clock_t t1, t2;
+	t1 = clock();
+	ShakeSort(a, n);
+	t2 = clock();
+	double d = double(t2 - t1)/ CLOCKS_PER_SEC;
+	cout << d;
+	ShakeSort(a, n, coutAssign1, countcompera1);
+	cout << coutAssign1 <<"  " << countcompera1;
+	delete[]a;
 	system("pause");
 	return 0;
 }
